8f8c03378350316017f01dcf6cf98791bdddc859
Malchiram <storeflash04@gmail.com>
2024-11-13 05:56:41 +0700

    fix emails

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const About = lazy(() => import("./component/About"))
 const Projects = lazy(() => import("./component/Projects"))
 const Contact = lazy(() => import("./component/Contact"))
 const StarsCanvas = lazy(() => import("./component/canvas/Stars"))
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export default function App() {
   return (
     <BrowserRouter>
@@ -22,6 +22,7 @@ export default function App() {
             <Contact />
             <StarsCanvas />
           </div>
+          <SpeedInsights />
         </div>
     </BrowserRouter>
 
